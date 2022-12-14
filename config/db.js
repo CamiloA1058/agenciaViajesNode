4bc1128441b0bd7c9fc42d45a6f0299b08319fba
Rango <rango@MacBook-Air-de-Rango.local>
2022-12-14 14:56:34 -0500

    Primer commit

---
+++ config/db.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv/config'
+
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = process.env;
+
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
+    port: '3306',
+    dialect: 'mysql',
+    define: {
+        timestamps: false,
+    },
+    pool:{
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    operatorAliases: false
+});
+export default db;
